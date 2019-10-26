875ba023a79643d0389125bdd80d618a98d12ef5
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2019-10-26 11:39:57 +0300

    Add css

--- src/main/java/com/akadatsky/GreetingController.java
+++ src/main/java/com/akadatsky/GreetingController.java
@@ -2,8 +2,6 @@ package com.akadatsky;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
@@ -14,24 +12,5 @@ public class GreetingController {
     public String main(Map<String, Object> model) {
         return "main";
     }
-
-    @GetMapping("/hello")
-    public String hello(
-            @RequestParam(name = "name", required = false, defaultValue = "World") String name,
-            @RequestParam(name = "age", required = false, defaultValue = "22") int age,
-            Map<String, Object> model
-    ) {
-
-        model.put("name", name);
-        model.put("age", age);
-        return "hello";
-    }
-
-    @PostMapping
-    public String test(Map<String, Object> model) {
-        return "test post";
-    }
-
-
 
 }
