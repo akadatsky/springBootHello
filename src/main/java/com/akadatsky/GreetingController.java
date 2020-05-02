4368179b65d220e66905fa135c46587a4dfd79af
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2020-05-02 11:04:17 +0300

    Fix css

--- src/main/java/com/akadatsky/GreetingController.java
+++ src/main/java/com/akadatsky/GreetingController.java
@@ -2,13 +2,16 @@ package com.akadatsky;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 
 @Controller
+@RequestMapping("/hello")
 public class GreetingController {
 
     @GetMapping
+    //@GetMapping("/hello")
     public String main(Map<String, Object> model) {
         return "main";
     }
